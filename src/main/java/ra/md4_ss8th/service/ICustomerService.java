0240c529104f359ff32779fe9e08b6412990ce44
c210214 <92442535+C210214@users.noreply.github.com>
2023-11-13 07:01:00 +0700

    first commit

---
+++ src/main/java/ra/md4_ss8th/service/ICustomerService.java
@@ -0,0 +1,19 @@
+package ra.md4_ss8th.service;
+
+
+import ra.md4_ss8th.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+
+}
